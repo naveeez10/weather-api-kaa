7db717fcee35501de48d5faba46a79addaaa097b
naveeez10 <naviz@incubyte.co>
2024-03-07 14:44:05 +0530

    change api key

--- src/main/java/com/weather/com/WeatherApi.java
+++ src/main/java/com/weather/com/WeatherApi.java
@@ -3,11 +3,9 @@ package com.weather.com;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.client.annotation.Client;
-import jakarta.inject.Singleton;
 
 @Client("https://api.weatherapi.com/v1/")
 public interface WeatherApi {
-
-    @Get("current.json?key=88ac638e9c024faca3c51331242202&q={q}&aqi=no")
-    Weather getWeather(@QueryValue("q") String zip);
+  @Get("current.json?key=f3a0c75f9c35485098263516240703&q={q}&aqi=no")
+  Weather getWeather(@QueryValue("q") String query);
 }
