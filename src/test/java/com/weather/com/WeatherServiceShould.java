ab1374549ed1fb7c9e86cf3d00a4e21000119f07
naveeez10 <naviz@incubyte.co>
2024-03-07 14:47:21 +0530

    remove unused imports

--- src/test/java/com/weather/com/WeatherServiceShould.java
+++ src/test/java/com/weather/com/WeatherServiceShould.java
@@ -1,14 +1,11 @@
 package com.weather.com;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,14 +13,17 @@ class WeatherServiceShould {
 
   WeatherService weatherService;
   @Mock WeatherApi weatherApi;
+
   @BeforeEach
   void setUp() {
     weatherService = new WeatherService(weatherApi);
   }
 
   @Test
-  @DisplayName("should call weather client to retrieve realtime weather data from the api through zip code")
-  void should_call_weather_client_to_retrieve_realtime_weather_data_from_the_api_through_zip_code() {
+  @DisplayName(
+      "should call weather client to retrieve realtime weather data from the api through zip code")
+  void
+      should_call_weather_client_to_retrieve_realtime_weather_data_from_the_api_through_zip_code() {
     weatherService.getWeatherByZip("01581");
     verify(weatherApi).getWeather("01581");
   }
